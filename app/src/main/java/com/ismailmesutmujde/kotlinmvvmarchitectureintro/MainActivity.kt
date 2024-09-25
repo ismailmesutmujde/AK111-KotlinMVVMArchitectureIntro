96a7ab5e95bd63cb00350a8608bf88686ad5120c
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-25 20:00:12 +0300

    initial commit

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinmvvmarchitectureintro/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ismailmesutmujde.kotlinmvvmarchitectureintro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        e
+        setContentView(R.layout.activity_main)
+    }
+}
