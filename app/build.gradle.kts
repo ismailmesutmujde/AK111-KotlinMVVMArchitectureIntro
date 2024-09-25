4de73ecb50b0715f37734e17830eed8919464f1e
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-25 20:17:18 +0300

    updated: ui design and coding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinmvvmarchitectureintro"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinmvvmarchitectureintro"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinmvvmarchitectureintro/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinmvvmarchitectureintro/MainActivity.kt
@@ -2,12 +2,60 @@ package com.ismailmesutmujde.kotlinmvvmarchitectureintro
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import com.ismailmesutmujde.kotlinmvvmarchitectureintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        e
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        bindingMain.textViewResult.text = "0"
+
+        bindingMain.buttonSum.setOnClickListener {
+            val receivedNumber1 = bindingMain.editTextNumber1.text.toString()
+            val receivedNumber2 = bindingMain.editTextNumber2.text.toString()
+
+            val number1 = receivedNumber1.toDouble()
+            val number2 = receivedNumber2.toDouble()
+
+            val sum = number1 + number2
+            bindingMain.textViewResult.text = sum.toString()
+        }
+
+        bindingMain.buttonSubtract.setOnClickListener {
+            val receivedNumber1 = bindingMain.editTextNumber1.text.toString()
+            val receivedNumber2 = bindingMain.editTextNumber2.text.toString()
+
+            val number1 = receivedNumber1.toDouble()
+            val number2 = receivedNumber2.toDouble()
+
+            val subtract = number1 - number2
+            bindingMain.textViewResult.text = subtract.toString()
+        }
+
+        bindingMain.buttonMultiply.setOnClickListener {
+            val receivedNumber1 = bindingMain.editTextNumber1.text.toString()
+            val receivedNumber2 = bindingMain.editTextNumber2.text.toString()
+
+            val number1 = receivedNumber1.toDouble()
+            val number2 = receivedNumber2.toDouble()
+
+            val multiply = number1 * number2
+            bindingMain.textViewResult.text = multiply.toString()
+        }
+        bindingMain.buttonDivide.setOnClickListener {
+            val receivedNumber1 = bindingMain.editTextNumber1.text.toString()
+            val receivedNumber2 = bindingMain.editTextNumber2.text.toString()
+
+            val number1 = receivedNumber1.toDouble()
+            val number2 = receivedNumber2.toDouble()
+
+            val divide = number1 / number2
+            bindingMain.textViewResult.text = divide.toString()
+        }
+
     }
 }
